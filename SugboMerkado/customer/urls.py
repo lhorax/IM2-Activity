1db0626b454702adb3e6c94619c1b62c75a6bd1e
Gamerlem <lemjayllanos@gmail.com>
2020-09-22 11:31:55 +0800

    read-update-image

--- SugboMerkado/customer/urls.py
+++ SugboMerkado/customer/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 app_name = 'customer'
 urlpatterns = [ 
@@ -9,3 +12,4 @@ urlpatterns = [
     path('index', views.CustomerIndexView.as_view(), name="index_view"),
     path('registration', views.CustomerRegistrationView.as_view(), name="registration_view"),
 ]
+urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
